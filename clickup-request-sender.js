481a9a7e07d1d8ed6132ce6f3a4e97b64f375d2e
sosmii <setsuya.osumi.private@gmail.com>
2019-02-25 13:23:36 +0900

    定数定義を修正

--- clickup-request-sender.js
+++ clickup-request-sender.js
@@ -4,12 +4,13 @@ const request = require('request-promise');
 const BASE_URL = 'https://api.clickup.com/api/v1/team';
 const TEAM_ID = process.env.TEAM_ID;
 const MY_USER_ID = process.env.MY_USER_ID;
+const USER_TOKEN = process.env.USER_TOKEN;
 
 module.exports = async targetLists => {
   const options = {
     url: `${BASE_URL}/${TEAM_ID}/task`,
     headers: {
-      Authorization: process.env.USER_TOKEN
+      Authorization: USER_TOKEN
     },
     qs: {
       page: 0,
